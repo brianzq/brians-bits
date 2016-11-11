b463fce680066ecc5939d18663f2ae64158b7255
Brian Zhang <brian@luxe.com>
2016-11-10 21:42:32 -0800

    Initial commit

---
+++ mysite/apps/blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from mysite.apps.blog.models import Post
+
+
+admin.site.register(Post)
---
+++ mysite/apps/personal/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'personal/index.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html', {
+        'content': ['If you would like to contact me, please email me',
+                    'leapingzhang@gmail.com']
+    })
