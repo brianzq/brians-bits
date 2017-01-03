0fdea41e5716ad20e6d4fd587da9a467180afa19
Brian Zhang <brian@luxe.com>
2017-01-02 20:24:17 -0800

    Finish styling

--- mysite/apps/personal/static/js/main.js
+++ mysite/apps/personal/static/js/main.js
@@ -3,5 +3,22 @@
 $(document).ready(function() {
   // get current URL path and assign 'active' class
   var pathname = window.location.pathname;
-  $('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+  // $('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+  $(`.nav > li > a[href="${pathname}"]`).parent().addClass('active');
+});
+
+function setFooterStyle() {
+    var docHeight = $(window).height();
+    var footerHeight = $('#footer').outerHeight();
+    var footerTop = $('#footer').position().top + footerHeight;
+    if (footerTop < docHeight) {
+        $('#footer').css('margin-top', (docHeight - footerTop) + 'px');
+    } else {
+        $('#footer').css('margin-top', '');
+    }
+    $('#footer').removeClass('invisible');
+}
+$(document).ready(function() {
+    setFooterStyle();
+    window.onresize = setFooterStyle;
 });
