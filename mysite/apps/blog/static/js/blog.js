d4365151def3922655b147b757774811cf0eb630
Brian Zhang <leapingzhang@gmail.com>
2017-04-05 21:56:39 -0700

    Fix various bugs

--- mysite/apps/blog/static/js/blog.js
+++ mysite/apps/blog/static/js/blog.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
   'use strict';
 
-  console.log('action started.');
   $('.slide-down-fade-in')
-    .css('opacity', 0)
+    // .css('opacity', 0)
     .slideDown(1000)
     .animate(
       { opacity: 1 },
       { queue: false, duration: 1000 }
     );
-    console.log('action completed.');
 });
--- mysite/apps/personal/static/js/main.js
+++ mysite/apps/personal/static/js/main.js
@@ -3,7 +3,6 @@
 $(document).ready(function() {
   // get current URL path and assign 'active' class
   var pathname = window.location.pathname;
-  // $('.nav > li > a[href=''+pathname+'']').parent().addClass('active');
   $(`.nav > li > a[href='${pathname}']`).parent().addClass('active');
 });
 
@@ -14,7 +13,8 @@ function setFooterStyle() {
   if (footerTop < docHeight) {
       $('#footer').css('margin-top', (docHeight - footerTop) + 'px');
   } else {
-      $('#footer').css('margin-top', '');
+      // have some separation between content
+      $('#footer').css('margin-top', '50px');
   }
   $('#footer').removeClass('invisible');
 }
