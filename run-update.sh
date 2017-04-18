f00ffec36529467a1a1a2bc540c817f8e0c8efc0
Brian Zhang <leapingzhang@gmail.com>
2017-04-18 00:25:56 -0700

    Use new repo url in run-update

--- run-update.sh
+++ run-update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # build new docker image
-docker build -t brianzq/mysite https://github.com/brianzq/mysite.git
+docker build -t brianzq/brians-bits https://github.com/brianzq/brians-bits.git
 # remove previous containers
 docker rm -f $(docker ps -a -q)
 # run new docker image
-docker run -d -p 80:80 brianzq/mysite
+docker run -d -p 80:80 brianzq/brians-bits
